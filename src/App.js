25085054ec90e61e97f1e40a0f451aee8c992650
Niko Anzai <nikoanzai@gmail.com>
2022-05-15 09:27:53 +0300

    feat: implement API call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import { useState } from "react";
+import axios from "axios";
 
 import "./App.scss";
 
 function App() {
+  const [text, setText] = useState("");
   const [submitting, setSubmitting] = useState(false);
+  const [nonWords, setNonWords] = useState([]);
 
-  const handleSubmit = (e) => {
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
-    // TODO: implement API call
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL}/find-non-words`,
+        {
+          sentence: text,
+        }
+      );
+      setNonWords(data);
+    } catch (error) {
+      console.error(error);
+    }
+
+    setSubmitting(false);
   };
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input autoFocus type="text" className="word-input" />
+        <input
+          autoFocus
+          type="text"
+          className="word-input"
+          onChange={handleChangeText}
+        />
         <button disabled={submitting} type="submit" className="check-button">
           Check
         </button>
+        {!!nonWords.length && (
+          <p className="non-word-text">
+            Non-English words: {nonWords.join(", ")}
+          </p>
+        )}
       </form>
     </div>
   );

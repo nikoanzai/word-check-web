7969dbf98cbaeb8469aa1ab60ae911095d8e4020
Niko Anzai <nikoanzai@gmail.com>
2022-05-15 07:56:51 +0300

    feat: word check basic UI infra

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input autoFocus type="text" className="word-input" />
+        <button type="submit" className="check-button">
+          Check
+        </button>
+      </form>
     </div>
   );
 }
